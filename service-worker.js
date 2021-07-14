83feb0479262bca016315fa0f1a8a75ea0c5a03f
TRyanAir <35853495+TRyanAir@users.noreply.github.com>
2021-07-14 11:54:23 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4f4f07b2786f411c6e4864efe001bdc8.js"
+  "./precache-manifest.c696edddd74b458c610507678436cccb.js"
 );
 
 self.addEventListener('message', (event) => {
